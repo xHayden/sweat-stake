244e72a67eb478a7ab70556b137283831ad4e38b
Hayden Carpenter <haydencarp@outlook.com>
2023-11-24 14:17:55 -0500

    Initial Commit

---
+++ SweatStake/SweatStakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweatStakeApp.swift
+//  SweatStake
+//
+//  Created by Hayden Carpenter on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SweatStakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
